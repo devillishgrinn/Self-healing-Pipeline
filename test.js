c665014d7634d8bf1a243473c48fac776d471a3d
Drake Gordon <rishabhm4202@gmail.com>
2026-06-29 13:42:45 +0530

    testing wenhook response

--- test.js
+++ test.js
@@ -2,10 +2,10 @@
 //const express = require("express");
 //const http = require("http");
 
-//const app = express();
+const app = express();
 const PORT = 5000;
 
-//app.get("/", (req, res) => res.sendStatus(200));
+app.get("/", (req, res) => res.sendStatus(200));
 
 const server = app.listen(PORT, () => {
     http.get(`http://localhost:${PORT}`, (res) => {
